42c1c8c0952c252afeaeb4262a0028291e003b2d
TerminalTrickster <L.Morkel@Outlook.de>
2025-02-11 18:31:34 +0100

    edit vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/web-grau/',
+  base: '/',
   assetsInclude: ['**/*.JPG', '**/*.JPEG', '**/*.png', '**/*.gif', '**/*.svg'], // Add other image formats as needed
 });
